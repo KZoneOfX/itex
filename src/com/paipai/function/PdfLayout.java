624c26bd86f272c691d19e745a60478a2992a0eb
Xiaoke Zhang <kzoneofx@foxmail.com>
2016-03-12 15:44:52 +0800

    添加 pdf 转 jpg 功能， 引用 commons-logging-1.2.jar fontbox-1.8.11.jar pdfbox-1.8.11.jar

---
+++ src/com/paipai/function/PdfLayout.java
@@ -0,0 +1,25 @@
+package com.paipai.function;
+
+import com.itextpdf.text.DocumentException;
+import com.paipai.function.pdfLayoutCase.PdfLayoutCase;
+import java.io.IOException;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Xiaoke Zhang
+ * Date: 3/11/2016
+ * Time: 11:55 PM
+ * To change this template use File | Settings | File Templates.
+ * java -jar example.jar example.pdf n*m q  (表示将pdf排版成 每行n张,每列m张,q=1表示横向,q=0 表示纵向;)
+ * 1.1 1.2 1.4 1.9
+ */
+public class PdfLayout {
+
+    public static void main(String[] args) throws IOException, DocumentException {
+        PdfLayoutCase pdfLayoutCase = new PdfLayoutCase(args);
+        System.out.println(pdfLayoutCase.process());
+
+
+    }
+
+}
